ab9bdcd073754f7e721b3c8d73b63c7b8231b822
Carlomadella <madella871@gmail.com>
2025-05-30 00:23:35 +0200

    [FEAT] completata milestone 4 con bonus

--- controllers/postController.js
+++ controllers/postController.js
@@ -12,9 +12,41 @@ const index = (req, res) => {
   });
 };
 
-// metodo show del controller
-const show = (req,res) => {
-    console.log(`Dettaglio del post con id:${req.params.id}`)
+const show = (req, res) => {
+    const { id } = req.params;
+    
+    // creo la query per il recupero del singolo post
+    const postSql = "SELECT * FROM posts WHERE id = ?";
+    
+    // BONUS: creo la query per recuperare i tag del post che vogliamo visualizzare
+    const tagSql = `
+        SELECT *
+        FROM tags
+        JOIN post_tag ON post_tag.tag_id = tags.id
+        WHERE post_tag.post_id = ?
+    `;
+    
+    // eseguo la query
+    connection.query(postSql, [id], (err, postResult) => {
+        if(err) return res.status(500).json({error: "Database query failed: "+err});
+        
+        // controlliamo che il post cercato esista
+        if(postResult.length === 0) return res.status(404).json({error: "Post not found"})
+        
+        // recuperiamo il singolo post dall'array risultato
+        const post = postResult[0];
+        
+        // BONUS
+        // eseguo la seconda query per recuperare i tag del post trovato
+        connection.query(tagSql, [id], (err, tagsResult) => {
+            if(err) return res.status(500).json({error: "Database query failed: "+err});
+            
+            // nell'oggetto post vado a creare una nuova proprietà a cui assegnare l'array recuperato
+            post.tags = tagsResult;
+            
+            res.json(post);
+        })
+    })
 }
 
 // metodo delete del controller
